ba08f96f69b3472bf82972c54ac5c9fba9fa9c26
meow <meow@>
2022-03-21 02:29:05 -0700

    update

--- src/structures/BST/bst.ts
+++ src/structures/BST/bst.ts
@@ -1,33 +1,39 @@
 // ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
 // Types
 
-interface BST {
-   root: Node | null;
+interface 森 {
+   root: 木 | null;
    size: number;
 }
 
-interface Node {
+interface 木 {
    口: number
-   小: Node | undefined
-   大: Node | undefined
-}
-
-export default {
-   
+   小: 木 | undefined
+   大: 木 | undefined
 }
 
 // ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
 // Binary Search Tree (BST)
 
-class BST {
-   root: Node | undefined = undefined
-   size = 0
+class 森 {
+   constructor() {
+      this.root = null;
+      this.size = 0;
+   }
+
+   add(口: number) {
+      if (this.root === null) {
+         this.root = new 木(口)
+         this.size++
+         return
+      }
+   }
 }
 
 // ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
 // Atoms
 
-class Node {
+class 木 {
    constructor(value) {
       this.口 = value
       this.小 = undefined
@@ -37,8 +43,9 @@ class Node {
    insert(口) {
       if (口 === this.口) return false
       const next = 口 < this.口 ? '小' : '大'
-      if (this[next] === undefined) this[next] = new Node(口)
-      else this[next].insert(口)
+      this[next] === undefined 
+         ? this[next] = new 木(口)
+         : this[next].insert(口)
    }
 
    contains(口) {
@@ -47,3 +54,12 @@ class Node {
       return !!this[next]?.contains(口)
    }
 }
+
+// ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
+// Test
+
+let n = new 木(0)
+n.insert(-10)
+n.insert(-10)
+n.insert(10)
+n.insert(10)
