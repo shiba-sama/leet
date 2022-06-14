bcce73ae4084a287bd6bcc93df3a3b82d9bd67a7
meow <meow@>
2022-06-14 16:29:17 -0700

    update

---
+++ src/2.ts
@@ -0,0 +1,65 @@
+// —————————————————————————————————————————————————————————————————————————————
+// Environment
+
+type ListNode = {
+   val: BigInt
+   next: ListNode | null
+}
+
+function ListNode(val=0n, next:ListNode|null=null): ListNode {
+   return {
+      val,
+      next
+   }
+}
+
+function listToNumber(l:ListNode): BigInt {
+   let digits:BigInt[] = []
+   let curr:ListNode|null = l
+   while (curr) {
+      digits.unshift(curr.val)
+      curr = curr.next
+   }
+   return BigInt(digits.join(""))
+}
+
+function numberToList(n:BigInt): ListNode {
+   const [first, ...rest] = String(n).split("").map(BigInt).reverse()
+   let root = ListNode(first)
+   let curr = root
+
+   for (const digit of rest) {
+      curr.next = ListNode(digit)
+      curr = curr.next
+   }
+
+   return root
+}
+
+// —————————————————————————————————————————————————————————————————————————————
+// Solve
+
+function addTwoNumbers(l1:ListNode, l2:ListNode): ListNode {
+   const s1 = listToNumber(l1)
+   const s2 = listToNumber(l2)
+   const sum = s1 + s2
+   const [first, ...rest] = String(sum).split("").map(BigInt).reverse()
+
+   let root = ListNode(first)
+   let curr = root
+
+   for (const digit of rest) {
+      curr.next = ListNode(digit)
+      curr = curr.next
+   }
+
+   return root
+}
+
+// —————————————————————————————————————————————————————————————————————————————
+// Test
+
+let l1 = ListNode(9n, ListNode(2n, ListNode(3n)))               // 329
+let l2 = ListNode(4n, ListNode(5n, ListNode(6n, ListNode(7n)))) // 7654
+
+export default {}
