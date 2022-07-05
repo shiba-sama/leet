32a226400712aeb4f20d02b87077cce36a72c024
meow <meow@>
2022-07-05 12:37:38 -0700

    update

---
+++ src/bin/55.rs
@@ -0,0 +1,17 @@
+// —————————————————————————————————————————————————————————————————————————————
+// Solution
+
+fn can_jump(nums: Vec<i32>) -> bool {
+   let mut i = 0;
+   let mut d = 0;
+   while i < nums.len() && i <= d {
+      d = d.max(i + nums[i] as usize);
+      i += 1;
+   }
+   return i == nums.len();
+}
+
+fn main() {
+   println!("Can jump: {}", can_jump(vec![2, 3, 1, 1, 4]));
+   println!("Can jump: {}", can_jump(vec![3, 2, 1, 0, 4]));
+}
