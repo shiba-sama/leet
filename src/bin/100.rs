58cc95d9352abf3a91ebc940b88a0b3188ac91a4
meow <meow@>
2022-07-07 00:02:25 -0700

    update

---
+++ src/bin/100.rs
@@ -0,0 +1,45 @@
+// —————————————————————————————————————————————————————————————————————————————
+// Environment
+
+use std::rc::Rc;
+use std::cell::RefCell;
+
+pub struct TreeNode {
+  pub val: i32,
+  pub left: Option<Rc<RefCell<TreeNode>>>,
+  pub right: Option<Rc<RefCell<TreeNode>>>,
+}
+
+impl TreeNode {
+  #[inline]
+  pub fn new(val: i32) -> Self {
+    TreeNode {
+      val,
+      left: None,
+      right: None
+    }
+  }
+}
+
+type BST = Option<Rc<RefCell<TreeNode>>>;
+
+// —————————————————————————————————————————————————————————————————————————————
+// Solve
+
+pub fn is_same_tree(一: BST, 二: BST) -> bool {
+   match (一, 二) {
+      (Some(一), Some(二)) => {
+         let 一 = 一.borrow();
+         let 二 = 二.borrow();
+         一.val == 二.val 
+            && is_same_tree(一.left.clone(), 二.left.clone()) 
+            && is_same_tree(一.right.clone(), 二.right.clone())
+      },
+      (None, None) => true,
+      _ => false
+   }
+}
+
+fn main() {
+
+}
