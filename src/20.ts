dd8a19270db0c2e42aa8a128dc32a388832a967c
meow <meow@>
2022-07-17 09:50:03 -0700

    update

---
+++ src/20.ts
@@ -0,0 +1,10 @@
+function isValid(parens:string) {
+   if (parens.length % 2 !== 0) return false
+   const pairs = { '(': ')', '[': ']', '{': '}' }
+   const stack:string[] = []
+   for (const p of parens) {
+      if (p in pairs) stack.push(p)
+      else if (!stack.length || pairs[stack.pop()!] !== p) return false
+   }
+   return stack.length === 0
+}
