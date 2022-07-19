e7a740a6282a2ac75823523803ff5631949ec168
shiba <shiba@laptop>
2022-07-18 19:14:05 -0700

    update

--- src/structures/Linked List/oo.ts
+++ src/structures/Linked List/oo.ts
@@ -1,19 +1,4 @@
 // ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
-// Type
-
-interface Node {
-   value: any
-   next: Node | undefined // undefined if last
-   constructor(value?: any, next?: Node)
-}
-
-interface List {
-   root: Node | undefined // undefined if 0
-   size: number
-   newList(...args): [Node, Node]
-}
-
-// ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
 // Utility
 
 const none = Symbol("none")
@@ -22,6 +7,8 @@ const none = Symbol("none")
 // Atom
 
 class Node {
+   value:any
+   next?:Node|undefined
    constructor(value:any=none, next?:Node) {
       this.value = value
       this.next = next
@@ -32,20 +19,19 @@ class List {
    root:Node|undefined = undefined
    size:number = 0
 
-   unshift(value: any) {
+   unshift(value:any) {
       this.root = new Node(value, this.root)
       return ++this.size
    }
 
    shift() {
-      if (this.size === 0) return undefined
       const root = this.root
-      this.root = root?.next // undefined if last
+      this.root = root?.next
       this.size--
       return root?.value
    }
 
-   get(i: number) {
+   get(i:number) {
       let curr = this.root
       while (i--) curr = curr?.next
       return curr?.value
