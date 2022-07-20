05ea56fd59d669aca176cb803492814e07b16466
meow <meow@>
2022-07-20 13:25:33 -0700

    update

--- src/structures/Heap/binary_heap_oo.ts
+++ src/structures/Heap/binary_heap_oo.ts
@@ -7,34 +7,55 @@ If starting index is `1`:
 
 If starting index is `0`:
    - parent of a node at `n` is `(n - 1) >> 1`
-   
+
 */
 
-class MinBinHeap<T> {
-   品: T[] = []
+class MinBinHeap {
+   品: number[] = [NaN]
 
-   #swap(一:number, 二:number) { [this.品[一], this.品[二]] = [this.品[二], this.品[一]] }
+   get top() { return this.品[1] }
+   get length() { return this.品.length - 1 }
+   get serial() { return this.品.slice(1) }
 
-   #up(下:number) {
-      let 上 = 下 >> 1
-      while (1 < 上 && this.品[下] < this.品[上]) {
-         this.#swap(下, 上)
-         上 >>= 1
+   #swap(一:number, 二:number) {
+      [this.品[一], this.品[二]] = [this.品[二], this.品[一]]
+   }
+
+   #up() {
+      let i = this.length
+      while (i > 0 && this.品[i] < this.品[i >> 1]) {
+         this.#swap(i, i >> 1)
+         i >>= 1
       }
    }
 
-   in(口: T) {
-      this.品.push(口)
-      this.#up(this.品.length - 1)
+   #down() {
+      let i = 1
+      while (i < this.length) {
+         let left = i << 1
+         let right = left + 1
+         
+      }
    }
 
-   top(): T { return this.品[0] }
+   in(口:number) {
+      this.品.push(口)
+      this.#up()
+   }
+
+   out() {
+      if (this.length === 0) return undefined
+      let top = this.top
+      this.品[1] = this.品.pop()!
+      this.#down()
+      return top
+   }
 }
 
-const b = new MinBinHeap<number>()
+const b = new MinBinHeap()
 b.in(4)
 b.in(1)
 b.in(3)
-b.in(2)
 b.in(0)
+b.in(2)
 b.in(5)
--- src/structures/Heap/pairingHeap.ts
+++ src/structures/Heap/pairingHeap.ts
@@ -1,15 +1,52 @@
-class Node {
-   val: any
-   kids: { [key: string]: Node }
+class Node<T> {
+   口: T
+   kids: Node<T>[] = []
 
-   constructor(val) {
-      this.val = val
-      this.kids = {}
+   constructor(口:T) { this.口 = 口 }
+}
+
+class MinPairingHeap<T> {
+   root: Node<T>|undefined = undefined
+   size: number = 0
+
+   get min() { return this.root && this.root.口 }
+
+   #merge(一:Node<T>|undefined, 二:Node<T>|undefined) {
+      if (!一) return 二
+      if (!二) return 一
+      if (一.口 < 二.口) {
+         一.kids.push(二)
+         return 一
+      }
+      二.kids.push(一)
+      return 二
+   }
+
+   #mergeKids(kids:Node<T>[]): Node<T>|undefined {
+      if (kids.length === 0) return undefined
+      if (kids.length === 1) return kids[0]
+      const [一, 二, ...rest] = kids
+      return this.#merge(this.#merge(一, 二), this.#mergeKids(rest))
+   }
+
+   in(口:T) {
+      this.root = this.#merge(this.root, new Node(口))
+      return ++this.size
+   }
+
+   out() {
+      if (!this.root) return undefined
+      const top = this.root.口
+      this.root = this.#mergeKids(this.root.kids)
+      this.size--
+      return top
    }
 }
 
-class Heap {
-   root: Node = null
-
-   
-}
+let h = new MinPairingHeap()
+h.in(4)
+h.in(1)
+h.in(3)
+h.in(0)
+h.in(2)
+h.in(5)
