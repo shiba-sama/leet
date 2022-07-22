687c38b81a88c81ac2210224b24f25ef169e99a1
meow <meow@>
2022-07-21 17:13:52 -0700

    update

--- src/structures/Array/quickSelect.ts
+++ src/structures/Array/quickSelect.ts
@@ -1,13 +1,12 @@
 // —————————————————————————————————————————————————————————————————————————————
-// Environment
-
-function swap(arr:number[], i:number, j:number) {
-   [arr[i], arr[j]] = [arr[j], arr[i]];
-}
-
-function λ(a:number, b:number) {
-   return Math.sign(a - b)
-}
-
-// —————————————————————————————————————————————————————————————————————————————
 // Quick Select
+
+function quickSelect<T>(arr:T[], kth:number): T {
+   const [中, ...品] = arr
+   const 小 = 品.filter(口 => 口 < 中)
+   const 大 = 品.filter(口 => 口 >= 中)
+
+   if (小.length === kth) return 中
+   if (小.length > kth) return quickSelect(小, kth)
+   return quickSelect(大, kth - 小.length - 1)   
+}
