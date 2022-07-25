05a3bf14e061fa0516520c5169115b7059b5dcc0
meow <meow@>
2022-07-25 00:21:42 -0700

    update

--- src/structures/Array/permutation.ts
+++ src/structures/Array/permutation.ts
@@ -1,8 +1,17 @@
-function * permute(a, n = a.length) {
-   if (n <= 1) yield a.slice();
+// ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
+// Permutation
+
+function * permute<T>(arr:T[], n = arr.length): IterableIterator<T[]> {
+   if (n <= 1) yield arr
    else for (let i = 0; i < n; i++) {
-      yield * permute(a, n - 1);
+      yield * permute(arr, n - 1)
       const j = n % 2 ? 0 : i;
-      [a[n-1], a[j]] = [a[j], a[n-1]];
+      [arr[n-1], arr[j]] = [arr[j], arr[n-1]]
    }
 }
+
+// ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
+// Test
+
+let permutations = permute([1, 2, 3])
+for (let p of permutations) console.log(p)
