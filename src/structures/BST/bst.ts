d1a4ae37c0477179d94e262fb10f994ed4b530c4
meow <meow@>
2022-07-24 23:00:03 -0700

    update

--- src/structures/BST/bst.ts
+++ src/structures/BST/bst.ts
@@ -1,59 +1,21 @@
 // ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
-// Types
+// Binary Search Tree (BST)
 
-interface Tree {
-   root?: Node;
-   size: number;
-}
-
-interface Node {
+class Node {
    口: number
    small?: Node
    big?: Node
-}
 
-// ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
-// Binary Search Tree (BST)
-
-class Tree {
-   constructor() {
-      this.root = undefined;
-      this.size = 0;
-   }
-
-   add(口: number) {
-      if (this.root === undefined) {
-         this.root = new Node(口)
-         return ++this.size
-      }
-
-      if (!this.root.add(口)) return this.size
-      else return ++this.size
-   }
-
-   contains(口: number) {
-      return this.root === undefined
-         ? false
-         : this.root.has(口)
-   }
-}
-
-// ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
-// Atoms
-
-class Node {
-   constructor(口: number) {
-      this.口 = 口
-   }
+   constructor(口: number) { this.口 = 口 }
 
    add(口: number) {
       if (口 === this.口) return false
       const next = 口 < this.口 ? 'small' : 'big'
       if (this[next] === undefined) this[next] = new Node(口)
-      else this[next].add(口)
+      else this[next]!.add(口)
    }
 
-   has(口: number) {
+   has(口: number): boolean {
       if (口 === this.口) return true
       const next = 口 < this.口 ? 'small' : 'big'
       return !!this[next]?.has(口)
