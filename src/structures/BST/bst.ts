679d834fa28caf0c59d80152fc4412e00b60df54
meow <meow@>
2022-07-24 23:48:12 -0700

    update

--- src/structures/BST/bst.ts
+++ src/structures/BST/bst.ts
@@ -14,14 +14,15 @@ class Node {
    constructor(口:number) { this.口 = 口 }
 
    add(口:number) {
-      if (口 === this.口) return false
+      if (口 === this.口) return
       const next = 口 < this.口 ? '小' : '大'
       if (this[next] === undefined) this[next] = new Node(口)
       else this[next]!.add(口)
    }
 
    has(口:number): boolean {
-      return 口 === this.口 || !!this[口 < this.口 ? '小' : '大']?.has(口)
+      return 口 === this.口 
+         || !!this[口 < this.口 ? '小' : '大']?.has(口)
    }
 }
 
