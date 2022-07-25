85c86b966e55554f14fd7b14b7c3080a4e722595
meow <meow@>
2022-07-24 23:34:17 -0700

    update

--- src/structures/BST/bst.ts
+++ src/structures/BST/bst.ts
@@ -1,24 +1,27 @@
 // ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
-// Binary Search Tree (BST)
+// Types
+
+interface Node {
+   口: number
+   小?: Node
+   大?: Node
+}
+
+// ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
+// Atoms
 
 class Node {
-   口: number
-   small?: Node
-   big?: Node
+   constructor(口:number) { this.口 = 口 }
 
-   constructor(口: number) { this.口 = 口 }
-
-   add(口: number) {
+   add(口:number) {
       if (口 === this.口) return false
-      const next = 口 < this.口 ? 'small' : 'big'
+      const next = 口 < this.口 ? '小' : '大'
       if (this[next] === undefined) this[next] = new Node(口)
       else this[next]!.add(口)
    }
 
-   has(口: number): boolean {
-      if (口 === this.口) return true
-      const next = 口 < this.口 ? 'small' : 'big'
-      return !!this[next]?.has(口)
+   has(口:number): boolean {
+      return 口 === this.口 || !!this[口 < this.口 ? '小' : '大']?.has(口)
    }
 }
 
