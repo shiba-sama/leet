9311fab7d4f277197de1a2950de42079a5331b07
meow <meow@>
2022-07-26 17:02:37 -0700

    update

--- src/structures/Heap/binary_heap_oo.ts
+++ src/structures/Heap/binary_heap_oo.ts
@@ -15,7 +15,7 @@ class MinBinHeap {
 
    get top() { return this.品[1] }
    get length() { return this.品.length - 1 }
-   get serial() { return this.品.slice(1) }
+   get serialize() { return this.品.slice(1) }
 
    #swap(一:number, 二:number) {
       [this.品[一], this.品[二]] = [this.品[二], this.品[一]]
@@ -23,28 +23,30 @@ class MinBinHeap {
 
    #up() {
       let i = this.length
-      while (i > 0 && this.品[i] < this.品[i >> 1]) {
-         this.#swap(i, i >> 1)
-         i >>= 1
-      }
+      while (i > 1 && this.品[i] < this.品[i >> 1]) this.#swap(i, i >>= 1)
    }
 
-   #down() {
-      let i = 1
-      while (i < this.length) {
-         let left = i << 1
-         let right = left + 1
-         
+   #down(i = 1) {
+      const left = i << 1
+      const right = left + 1
+      let largest = i
+
+      if (left <= this.length && this.品[left] < this.品[largest]) largest = left
+      if (right <= this.length && this.品[right] < this.品[largest]) largest = right
+      if (largest !== i) {
+         this.#swap(i, largest) 
+         this.#down(largest)
       }
    }
 
    in(口:number) {
-      this.品.push(口)
+      this.品.push(口),
       this.#up()
    }
 
    out() {
       if (this.length === 0) return undefined
+      if (this.length === 1) return this.品.pop()
       let top = this.top
       this.品[1] = this.品.pop()!
       this.#down()
