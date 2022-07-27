293e67e3ce1f34c73f31af6c08d327c2f1756cd0
meow <meow@>
2022-07-26 23:06:35 -0700

    update

--- src/structures/Heap/binaryHeap.ts
+++ src/structures/Heap/binaryHeap.ts
@@ -6,7 +6,7 @@ class BinaryHeap {
    #λ: (a:number, b:number) => boolean
 
    get top() { return this.品[1] }
-   get length() { return this.品.length - 1 }
+   get size() { return this.品.length - 1 }
    get serialize() { return this.品.slice(1) }
 
    constructor(λ = (a:number, b:number) => a < b) { this.#λ = λ }
@@ -16,7 +16,7 @@ class BinaryHeap {
    }
 
    #up() {
-      let i = this.length
+      let i = this.size
       while (i > 1 && this.#λ(this.品[i], this.品[i >> 1])) this.#swap(i, i >>= 1)
    }
 
@@ -24,8 +24,8 @@ class BinaryHeap {
       const L = i << 1
       const R = L + 1
       let best = i
-      if (L <= this.length && this.#λ(this.品[L], this.品[best])) best = L
-      if (R <= this.length && this.#λ(this.品[R], this.品[best])) best = R
+      if (L <= this.size && this.#λ(this.品[L], this.品[best])) best = L
+      if (R <= this.size && this.#λ(this.品[R], this.品[best])) best = R
       if (best !== i) this.#swap(i, best), this.#down(best)
    }
 
@@ -35,8 +35,8 @@ class BinaryHeap {
    }
 
    out() {
-      if (this.length === 0) return undefined
-      if (this.length === 1) return this.品.pop()
+      if (this.size === 0) return undefined
+      if (this.size === 1) return this.品.pop()
       let top = this.top
       this.品[1] = this.品.pop()!
       this.#down()
