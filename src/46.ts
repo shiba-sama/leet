0d45dd3572bb93bc914043b215e371318948d506
meow <meow@>
2022-07-29 17:45:21 -0700

    update

--- src/46.ts
+++ src/46.ts
@@ -1,17 +1,19 @@
 // ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
-// Permutation
+// Environment
 
 /** Yield permutations through Heap's Method. */
-function * permute<T>(arr:T[], n = arr.length): IterableIterator<T[]> {
+function * p<T>(arr:T[], n = arr.length): IterableIterator<T[]> {
    if (n <= 1) yield arr.slice()
    else for (let i = 0; i < n; i++) {
-      yield * permute(arr, n - 1)
+      yield * p(arr, n - 1)
       const j = n % 2 ? 0 : i;
       [arr[n-1], arr[j]] = [arr[j], arr[n-1]]
    }
 }
 
 // ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
-// Test
+// All possible permutations
 
-let permutations = permute([1, 2, 3])
+function permute(arr:number[]) {
+   return Array.from(p(arr))
+}
