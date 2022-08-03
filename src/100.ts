64b63d844cd3f3a7644ce5a33ec3e605ad707b5d
meow <meow@>
2022-08-02 23:35:38 -0700

    meow

--- src/100.ts
+++ src/100.ts
@@ -18,9 +18,9 @@ type BST = TreeNode | null
 // Solve
 
 function isSameTree(一:BST, 二:BST) {
-   if (一 === null || 二 === null) return 一 === 二
-
-   return 一.val === 二.val
-      && isSameTree(一.left, 二.left)
-      && isSameTree(一.right, 二.right)
+   return 一 === null || 二 === null
+      ? 一 === 二
+      : 一.val === 二.val
+         && isSameTree(一.left, 二.left)
+         && isSameTree(一.right, 二.right)
 }
