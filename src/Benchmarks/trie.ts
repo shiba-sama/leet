dde6ac1ef5d6942c20b90d412be7d5660b3cf3a4
meowth <37882842+shiba-sama@users.noreply.github.com>
2022-08-12 11:59:50 -0700

    Trie update (#8)

--- src/Benchmarks/trie.ts
+++ src/Benchmarks/trie.ts
@@ -1,10 +1,20 @@
-import Trie from "../structures/Trie/anotherTrie.ts"
+import T0 from "../structures/Trie/anotherTrie.ts"
+import T1 from "../structures/Trie/trieClass.ts"
 import dictionary from "./data/dictionary.json" assert { type: "json" }
 
 Deno.bench(
-   "Trie",
+   "anotherTrie",
    () => {
-      const trie = new Trie()
-      for (const word of dictionary) trie.add(word)
+      const t = new T0()
+      for (const word of dictionary) t.add(word)
    }
 )
+
+Deno.bench(
+   "trieClass",
+   () => {
+      const t = new T1()
+      for (const word of dictionary) t.add(word)
+   }
+)
+
