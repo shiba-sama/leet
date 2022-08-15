42d5238e15b1ecd13c7bbf392871c4711c5de45d
meow <meow@>
2022-08-15 11:59:21 -0700

    update

--- src/Benchmarks/heap.ts
+++ src/Benchmarks/heap.ts
@@ -1,6 +1,5 @@
-import BinaryHeap0 from "../structures/Heap/binaryHeapClean.ts"
+import BinaryHeap0 from "../structures/Heap/binaryHeap0.ts"
 import BinaryHeap1 from "../structures/Heap/binaryHeap.ts"
-import PairingHeap from "../structures/Heap/pairingHeap.ts"
 import shuffledNats from "./data/nats.json" assert { type: "json" }
 import randomInts from "./data/randomInts.json" assert { type: "json" }
 
@@ -19,17 +18,6 @@ Deno.bench(
    "BinaryHeap1",
    () => {
       const heap = new BinaryHeap1<number>()
-      for (const n of shuffledNats) heap.in(n)
-      for (const n of heap.iter()) { }
-      for (const n of randomInts) heap.in(n)
-      for (const n of heap.iter()) { }
-   }
-)
-
-Deno.bench(
-   "PairingHeap",
-   () => {
-      const heap = new PairingHeap<number>()
       for (const n of shuffledNats) heap.in(n)
       for (const n of heap.iter()) { }
       for (const n of randomInts) heap.in(n)
