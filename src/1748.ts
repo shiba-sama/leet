c5e0c6c34b2b64f2b41a49df073a9e0e89c51355
meow <meow@>
2023-09-28 00:39:47 -0700

    update

--- src/1748.ts
+++ src/1748.ts
@@ -5,7 +5,7 @@ function sumOfUnique(nums:number[]) {
    const dict = new Map<number, number>()
    let sum = 0
 
-   for (const n of nums) dict.set(n, dict.get(n) ?? 0 + 1)
+   for (const n of nums) dict.set(n, (dict.get(n) ?? 0) + 1)
    for (const [k, v] of dict) if (v === 1) sum += k
 
    return sum
