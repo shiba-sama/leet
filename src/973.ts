4db734ef9b1f869875ed8f72020e6bbdb7c061d0
meow <meow@>
2023-09-28 22:39:12 -0700

    update

--- src/973.ts
+++ src/973.ts
@@ -3,13 +3,13 @@
 
 type point = [number, number]
 
-function dist(p:point) { return p[0]**2 + p[1]**2 }
+const len = (p:point) => p[0]**2 + p[1]**2
 
 // —————————————————————————————————————————————————————————————————————————————
 // K Largest Euclidian Norms
 
 function kClosest(points:point[], k:number): point[] {
    return points
-      .sort((a, b) => dist(a) - dist(b))
+      .sort((a, b) => len(a) - len(b))
       .slice(0, k)
 }
