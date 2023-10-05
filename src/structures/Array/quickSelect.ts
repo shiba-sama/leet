406dc6a0733dc90b5623de38b896706d1be500e6
meow <meow@>
2023-10-05 11:17:05 -0700

    update

--- src/structures/Array/quickSelect.ts
+++ src/structures/Array/quickSelect.ts
@@ -1,19 +1,19 @@
 // —————————————————————————————————————————————————————————————————————————————
 // Quick Select
 
-function quickSelect<T>(arr:T[], kth:number): T {
+function quickSelect<T>(arr:T[], k:number): T {
    const [中, ...品] = arr
    const 小 = 品.filter(口 => 口 < 中)
    const 大 = 品.filter(口 => 口 >= 中)
 
-   if (小.length === kth) return 中
-   if (小.length > kth) return quickSelect(小, kth)
-   return quickSelect(大, kth - 小.length - 1)   
+   if (小.length === k) return 中
+   if (小.length > k) return quickSelect(小, k)
+   return quickSelect(大, k - 小.length - 1)   
 }
 
 // —————————————————————————————————————————————————————————————————————————————
 // Quick Select
 
 const arr = [3,2,3,1,2,4,5,5,6]
-const kth = 4
-console.log(quickSelect(arr, kth))
+const k = 4
+console.log(quickSelect(arr, k))
