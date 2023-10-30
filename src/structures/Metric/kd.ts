7b3a9140bd82eb78c09f20e19aea831d31906048
meow <meow@>
2023-10-30 11:39:27 -0700

    update

--- src/structures/Metric/kd.ts
+++ src/structures/Metric/kd.ts
@@ -2,4 +2,56 @@
  A K-D tree for 2-dimensional data.
 */
 
-type Point = [number, number]
+type Point = {
+  x: number
+  y: number
+}
+
+interface Node {
+  point: Point
+  left: Node | null
+  right: Node | null
+}
+
+// —————————————————————————————————————————————————————————————————————————————
+// Utility
+
+function distance(一:Point, 二:Point) {
+  return (一.x - 二.x)**2 + (一.y - 二.y)**2
+}
+
+// —————————————————————————————————————————————————————————————————————————————
+// K-D Tree where K=2
+
+export class KDTree {
+  root: Node | null = null
+
+  insert(point: Point) {
+    if (this.root === null) {
+      this.root = { point, left: null, right: null }
+    } 
+    else {
+      this.#insertNode(this.root, point, 0)
+    }
+  }
+
+  #insertNode(node: Node, point: Point, depth: number) {
+    const dimension = depth % 2
+
+    if (point[dimension] < node.point[dimension]) {
+      if (node.left === null) {
+        node.left = { point, left: null, right: null }
+      } 
+      else {
+        this.#insertNode(node.left, point, depth + 1)
+      }
+    } else {
+      if (node.right === null) {
+        node.right = { point, left: null, right: null }
+      } 
+      else {
+        this.#insertNode(node.right, point, depth + 1)
+      }
+    }
+  }
+}
