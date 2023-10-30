880bc2416ad00659e3f00e44dd7b8ff53949d262
meow <meow@>
2023-10-30 11:52:40 -0700

    update

--- src/structures/Metric/kd.ts
+++ src/structures/Metric/kd.ts
@@ -16,7 +16,7 @@ interface Node {
 // —————————————————————————————————————————————————————————————————————————————
 // Utility
 
-const distance = (a:Point, b:Point) => (a.x - b.x)**2 + (a.y - b.y)**2
+const distance = (a: Point, b: Point) => (a.x - b.x) ** 2 + (a.y - b.y) ** 2
 
 // —————————————————————————————————————————————————————————————————————————————
 // K-D Tree where K=2
@@ -25,31 +25,20 @@ export class KDTree {
   root: Node | null = null
 
   insert(point: Point) {
-    if (this.root === null) {
-      this.root = { point, left: null, right: null }
-    } 
-    else {
-      this.#insertNode(this.root, point, 0)
-    }
+    if (this.root === null) this.root = { point, left: null, right: null }
+    else this.#insertNode(this.root, point, 0)
   }
 
   #insertNode(node: Node, point: Point, depth: number) {
     const dimension = depth % 2
 
     if (point[dimension] < node.point[dimension]) {
-      if (node.left === null) {
-        node.left = { point, left: null, right: null }
-      } 
-      else {
-        this.#insertNode(node.left, point, depth + 1)
-      }
-    } else {
-      if (node.right === null) {
-        node.right = { point, left: null, right: null }
-      } 
-      else {
-        this.#insertNode(node.right, point, depth + 1)
-      }
+      if (node.left === null) node.left = { point, left: null, right: null }
+      else this.#insertNode(node.left, point, depth + 1)
+    } 
+    else {
+      if (node.right === null) node.right = { point, left: null, right: null }
+      else this.#insertNode(node.right, point, depth + 1)
     }
   }
 }
