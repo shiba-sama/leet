2211c9eabceec50efa953b7cab647a04e2768a68
meow <meow@>
2023-10-30 12:14:20 -0700

    update

--- src/structures/Metric/kd.ts
+++ src/structures/Metric/kd.ts
@@ -25,21 +25,6 @@ const distance = (a: Point, b: Point) => (a.x - b.x)**2 + (a.y - b.y)**2
 export class KDTree {
   root: Node | null = null
 
-  insert(point: Point) {
-    if (this.root === null) this.root = { point, left: null, right: null }
-    else this.#insertNode(this.root, point, 0)
-  }
-
-  #insertNode(node: Node, point: Point, depth: number) {
-    const dimension = depth % 2
-
-    if (point[dimension] < node.point[dimension]) {
-      if (node.left === null) node.left = { point, left: null, right: null }
-      else this.#insertNode(node.left, point, depth + 1)
-    } 
-    else {
-      if (node.right === null) node.right = { point, left: null, right: null }
-      else this.#insertNode(node.right, point, depth + 1)
-    }
+  constructor(points: Point[]) {
   }
 }
