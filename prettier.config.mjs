19b123300e71aaaface77196963718c5911989d4
meow <meow@>
2023-10-30 11:22:18 -0700

    added kd tree

---
+++ prettier.config.mjs
@@ -0,0 +1,8 @@
+/** @type {import("prettier").Config} */
+export default {
+   tabWidth: 2,
+   useTabs: false,
+   semi: false,
+   printWidth: 90,
+   singleQuote: false,
+}
---
+++ src/structures/Metric/kd.ts
@@ -0,0 +1,5 @@
+/*
+ A K-D tree for 2-dimensional data.
+*/
+
+type Point = [number, number]
