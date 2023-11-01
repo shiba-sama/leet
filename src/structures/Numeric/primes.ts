741a656cbf850e772ecfd7736f6eefa6fdee7346
meow <meow@>
2023-11-01 15:38:00 -0700

    update

--- src/structures/Numeric/primes.ts
+++ src/structures/Numeric/primes.ts
@@ -3,12 +3,12 @@
 
 /** Returns array of all primes under `n` using the Sieve of Eratosthenes. */
 export function primes(n:number) {
-   const nats = Array.from({length: n}, () => true)
+   const nums = Array.from({length: n}, () => true)
    const primes = [] as number[]
 
    for (let i=2; i<Math.trunc(Math.sqrt(n)); i++)
-      if (nats[i]) for (let j=i**2; j<n; j+=i) nats[j] = false
+      if (nums[i]) for (let j=i**2; j<n; j+=i) nums[j] = false
 
-   for (let i=2; i<n; i++) if (nats[i]) primes.push(i)
+   for (let i=2; i<n; i++) if (nums[i]) primes.push(i)
    return primes
 }
