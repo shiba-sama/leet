834fc0b801bc46daebd6397bfa39e6bf0d8350cc
meow <meow@>
2023-11-04 21:39:37 -0700

    style update

--- src/structures/Heap/binaryHeap.ts
+++ src/structures/Heap/binaryHeap.ts
@@ -2,51 +2,70 @@
 // Binary Heap
 
 export default class BinaryHeap<T = number> {
-   品: T[] = [0 as unknown as T]
-   #λ: (一:T, 二:T) => boolean
+  品: T[] = []
+  #λ: (一: T, 二: T) => boolean
 
-   get top(): T | undefined { return this.品[1] }
-   get size() { return this.品.length - 1 }
-   get serialize() { return this.品.slice(1) }
+  get top(): T | undefined {
+    return this.品[1]
+  }
+  get size() {
+    return this.品.length - 1
+  }
+  get serialize() {
+    return this.品.slice(1)
+  }
 
-   constructor(λ = (一:T, 二:T) => 一 < 二) { this.#λ = λ }
+  constructor(λ = (一: T, 二: T) => 一 < 二, filler?: T) {
+    this.#λ = λ
+    this.品.push(filler ?? (0 as unknown as T))
+  }
 
-   #swap(一:number, 二:number) {
-      [this.品[一], this.品[二]] = [this.品[二], this.品[一]]
-   }
+  #swap(一: number, 二: number) {
+    ;[this.品[一], this.品[二]] = [this.品[二], this.品[一]]
+  }
 
-   #up() {
-      let i = this.size
-      while (1 < i && this.#λ(this.品[i], this.品[i >> 1])) this.#swap(i, i >>= 1)
-   }
+  #up() {
+    let i = this.size
+    while (1 < i && this.#λ(this.品[i], this.品[i >> 1])) 
+      this.#swap(i, (i >>= 1))
+  }
 
-   #down(i = 1) {
-      const L = i << 1
-      const R = L + 1
-      let 大 = i
-      if (L <= this.size && this.#λ(this.品[L], this.品[大])) 大 = L
-      if (R <= this.size && this.#λ(this.品[R], this.品[大])) 大 = R
-      if (大 !== i) this.#swap(i, 大), this.#down(大)
-   }
+  #down(i = 1) {
+    const L = i << 1
+    const R = L + 1
+    let 大 = i
+    if (L <= this.size && this.#λ(this.品[L], this.品[大])) 大 = L
+    if (R <= this.size && this.#λ(this.品[R], this.品[大])) 大 = R
+    if (大 !== i) this.#swap(i, 大), this.#down(大)
+  }
 
-   in(口:T) { this.品.push(口), this.#up() }
+  in(口: T) {
+    this.品.push(口), this.#up()
+  }
 
-   out() {
-      if (this.size === 0) return undefined
-      if (this.size === 1) return this.品.pop()!
-      const top = this.top
-      this.品[1] = this.品.pop()!
-      this.#down()
-      return top
-   }
+  out() {
+    switch (this.size) {
+      case 0:
+        return undefined
+      case 1:
+        return this.品.pop()!
+      default:
+        const top = this.top
+        this.品[1] = this.品.pop()!
+        this.#down()
+        return top
+    }
+  }
 
-   *iter() { for (let i = this.size; 0 < i; i--) yield this.out()! }
+  *iter() {
+    for (let i = this.size; 0 < i; i--) yield this.out()!
+  }
 }
 
 // —————————————————————————————————————————————————————————————————————————————
 // Test
 
-// const heap = new BinaryHeap<number>()
-// let arr = [10, 6, 4, 9, 1, 3, 8, 0, 2, 7, 5, 10, 0]
-// arr.forEach(n => heap.in(n))
-// const nums = Array.from(heap.iter())
+const heap = new BinaryHeap<number>()
+let arr = [10, 6, 4, 9, 1, 3, 8, 0, 2, 7, 5, 10, 0]
+arr.forEach((n) => heap.in(n))
+const nums = Array.from(heap.iter())
